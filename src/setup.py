6be2193b3c806fbb471e68255991d719c9b57e9f
Kyle Blue <kyle.blue.nuttall@gmail.com>
2020-04-28 14:44:23 +0100

    Added an installer

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+requirements_path = os.path.normpath(os.path.join(script_dir, "../requirements.txt"))
+
+requires = []
+with open(requirements_path) as req:
+    for line in req:
+        requires.append(line.strip())
+print(find_packages(script_dir))
+setup(
+    name="kubevarpkg",
+    packages=find_packages(script_dir),
+    version="0.1",
+    description="Bringing variable substitution to kubernetes! Both static and dynamic... Scrap Kustomize!",
+    install_requires=requires
+)
